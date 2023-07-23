8a605e4306ae14c09f1dbfacd55c5f34195762db
eppakayalanaresh <eppakayalanaresh08@gmail.com>
2023-07-24 01:18:53 +0530

    newpadding

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home=()=>{
     <div className=" h-fit md:h-screen  w-full bg-gradient-to-b  pt-9       bg-gradient-to-r from-green-400 to-blue-500          via-black to-gray-800" name="home">
     <div className="pt-32 md:pt-0 max-w-screen-lg  ml-5  md:mx-auto pt-5  h-full flex flex-col justify-center md:items-center  md:flex-row">
     <div className="flex flex-col justify-center ">
-        <h1 className="text-4xl sm:text-7xl font-bold text-white">I'm a Aspiring Full stack Developer</h1>
+        <h1 className="text-4xl sm:text-7xl font-bold mt-10 text-white">I'm a Aspiring Full stack Developer</h1>
         <p className="text-[#fdf4ff] py-5 max-w-md italic">Welcome to my portfolio website! I am Naresh Eppakayala, a dedicated and enthusiastic MERN full-stack developer with a passion for creating efficient, scalable solutions. Explore my projects and let's connect to bring your ideas to life</p>
         <div>
             <Link to="portfolio" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center justify-between bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md cursor-pointer">Portfolio
